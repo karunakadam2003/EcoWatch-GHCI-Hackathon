aaed36c6e7bb47d77a1e08d2a34da91840a8bc69
Anushka952 <kanushkar952.gmail.com>
2024-10-27 18:54:39 +0530

    Pin added

--- api/app.py
+++ api/app.py
@@ -1,10 +1,23 @@
+
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/set-location', methods=['POST'])
+def set_location():
+    data = request.json
+    lat = data.get('lat')
+    lng = data.get('lng')
+    
+    print(f"Received coordinates: Latitude={lat}, Longitude={lng}")
+    
+    return jsonify({"status": "success", "latitude": lat, "longitude": lng})
 
 if __name__ == '__main__':
     app.run(debug=True)
